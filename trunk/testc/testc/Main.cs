1621a2eef1160020b3d2ecb234adab4e23344091
nunu3 <kizuna.mhf@gmail.com>
2015-04-05 23:38:30 +0900

    change

--- trunk/testc/testc/Main.cs
+++ trunk/testc/testc/Main.cs
@@ -11,22 +11,30 @@ namespace testc
 		/// <param name="args">The command-line arguments.</param>
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("　               (:3[布団]");
+			Console.WriteLine ("　              (:3[布団]");
 			Console.ReadKey ();
+			Console.Clear ();
 			Console.WriteLine ("              (:3っ[布団]");
 			Console.ReadKey ();
+			Console.Clear ();
 			Console.WriteLine ("             (:3っ)[布団]");
 			Console.ReadKey ();
+			Console.Clear ();
 			Console.WriteLine ("           (:3っ)っ[布団]");
 			Console.ReadKey ();
+			Console.Clear ();
 			Console.WriteLine ("        (:3っ)っ -=[布団]");
 			Console.ReadKey ();
+			Console.Clear ();
 			Console.WriteLine ("      (:3っ)っ -=三[布団]");
 			Console.ReadKey ();
+			Console.Clear ();
 			Console.WriteLine ("    (:3っ)っ -=三三[布団]");
 			Console.ReadKey ();
+			Console.Clear ();
 			Console.WriteLine ("  (:3っ)っ -=三三三[布団]");
 			Console.ReadKey ();
+			Console.Clear ();
 			Console.WriteLine ("(:3っ)っ -=三三三三[布団]");
 			Console.ReadKey ();
 			Console.Clear ();
