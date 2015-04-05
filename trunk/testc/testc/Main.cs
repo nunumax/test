fb018d3fd24c58e4055e4f831b854bf486b0e08c
nunu3 <kizuna.mhf@gmail.com>
2015-04-05 17:10:54 +0900

    no message

--- trunk/testc/testc/Main.cs
+++ trunk/testc/testc/Main.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 
 namespace testc
 {
@@ -6,7 +7,8 @@ namespace testc
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			//Console.WriteLine ("Hello World!");
+			Process.Start(@"notepad.exe");
 		}
 	}
 }
