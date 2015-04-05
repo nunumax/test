a71918975604b8e83e4f3baaa75131155fac7e1f
nunu3 <kizuna.mhf@gmail.com>
2015-04-05 19:39:49 +0900

    no message

--- trunk/testc/testc/Main.cs
+++ trunk/testc/testc/Main.cs
@@ -5,6 +5,10 @@ namespace testc
 {
 	class MainClass
 	{
+		/// <summary>
+		/// The entry point of the program, where the program control starts and ends.
+		/// </summary>
+		/// <param name="args">The command-line arguments.</param>
 		public static void Main (string[] args)
 		{
 			//Console.WriteLine ("Hello World!");
